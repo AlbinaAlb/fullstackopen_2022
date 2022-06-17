83e1d98f9266b44cd7c143036e50ec81be80fa9a
AlbinaAlb <albinakovalevaa@gmail.com>
2022-06-17 13:57:19 +0300

    3.10

---
+++ part3/phonebook-backend/README.md
@@ -0,0 +1,5 @@
+##phonebook:
+
+
+
+https://fierce-hollows-37652.herokuapp.com/api/persons
--- part3/phonebook-frontend/src/services/persons.js
+++ part3/phonebook-frontend/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = 'https://fierce-hollows-37652.herokuapp.com/api/persons'
 
 const getPersons =  () => {
   const request = axios.get(baseUrl)
