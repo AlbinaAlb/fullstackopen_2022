810568f60ab054a951441e4e5fcd032e7ed22e0c
AlbinaAlb <albinakovalevaa@gmail.com>
2022-07-11 11:50:16 +0300

    complete 5.1-5.4

---
+++ part5/bloglist-frontend/src/App.js
@@ -0,0 +1,92 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import BlogForm from './components/BlogForm'
+import LoginForm from './components/LoginForm'
+import Notification from './components/Notification'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [message, setMessage] = useState(null)
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then((blogs) => setBlogs(blogs))
+  }, [])
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setMessage(null)
+    }, 5000)
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [message])
+  
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const handleLogin = async ( username, password ) => {
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      })
+      blogService.setToken(user.token)
+      setUser(user)
+      window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
+      setMessage(`You are logged in as ${username}`)
+    } catch (exception) {
+       setMessage('error ' + exception.response.data.error)
+    }
+  }
+
+   const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
+  } 
+
+  const createBlog = async (title, author, url) => {
+    try {
+      const blog = await blogService.create({
+        title,
+        author,
+        url,
+      })
+      setBlogs(blogs.concat(blog))
+      setMessage(`A new blog ${title} by ${author} added`)
+    } catch (exception) {
+      setMessage('error ' + exception.response.data.error)
+    }
+  }
+
+  return (
+    <div>
+      <Notification message={message} />
+      {user === null ? (
+        <LoginForm handleLogin={handleLogin} />
+      ) : (
+        <div>
+          <div>
+            <span>{user.name} logged-in</span>
+            <button onClick={handleLogout}>logout</button>
+          </div>
+          <BlogForm createBlog={createBlog} />
+          <h2>blogs</h2>
+          {blogs.map((blog) => (
+            <Blog key={blog.id} blog={blog} />
+          ))}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
