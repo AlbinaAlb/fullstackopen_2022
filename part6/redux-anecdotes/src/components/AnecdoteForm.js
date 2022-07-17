c3d4d730d28c240412f2deeb84a8f05a297446ca
AlbinaAlb <albinakovalevaa@gmail.com>
2022-07-17 12:49:04 +0300

    complete 6.1-6.8

---
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -0,0 +1,26 @@
+import { useDispatch } from 'react-redux'
+import { createAnecdote } from '../reducers/anecdoteReducer'
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch()
+
+  const handleCreateAnecdote = (event) => {
+    event.preventDefault()
+    const anecdote = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    dispatch(createAnecdote(anecdote))
+  }
+  return (
+    <div>
+      <h2>create new anecdote</h2>
+      <form onSubmit={handleCreateAnecdote}>
+        <div>
+          <input name="anecdote" />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
---
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,30 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { addVote } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = () => {
+  //компонент получает доступ к store. Функция выбирает данные из redux-store
+  const anecdotes = useSelector((state) => state)
+  //возвращает ссылку на dispatch функцию из store Redux, можно использовать его для отправки действий
+  const dispatch = useDispatch()
+
+  const handleVote = (id) => {
+    dispatch(addVote(id))
+  }
+  return (
+    <div>
+      {anecdotes
+        .sort((a, b) => b.votes - a.votes)
+        .map((anecdote) => (
+          <div key={anecdote.id}>
+            <div>{anecdote.content}</div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => handleVote(anecdote.id)}>vote</button>
+            </div>
+          </div>
+        ))}
+    </div>
+  )
+}
+
+export default AnecdoteList
