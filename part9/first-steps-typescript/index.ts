31308c3eeaa841cb444f259bb450e685a2177402
AlbinaAlb <albinakovalevaa@gmail.com>
2022-08-16 14:42:18 +0300

    WebExercises and eslint 9.6-9.7

--- part9/first-steps-typescript/index.ts
+++ part9/first-steps-typescript/index.ts
@@ -1,6 +1,9 @@
 import express = require('express')
 import { calculateBmi } from './bmiCalculator'
+import { calculateExercises } from './trainingCalculator'
+
 const app = express()
+app.use(express.json())
 
 app.get('/', (_req, res) => {
   res.send('Hello Full Stack!')
@@ -18,7 +21,28 @@ app.get('/bmi', (req, res) => {
     })
   }
 
-  res.send({weight, height, bmi})
+  res.send({ weight, height, bmi })
+})
+
+app.post('/calculate', (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any,  @typescript-eslint/no-unsafe-assignment
+  const { daily_exercises, target }: any = req.body
+
+  if (target && daily_exercises) {
+    const isArray: boolean = Array.isArray(daily_exercises)
+
+    if (isArray) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+      daily_exercises.some((exercise: number) => isNaN(Number(exercise)))
+    } else {
+      return res.status(400).json({ error: 'malformatted parameters' })
+    }
+  } else {
+    return res.status(400).json({ error: 'parameters missing' })
+  }
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  const result = calculateExercises(target, daily_exercises)
+  return res.json(result)
 })
 
 const PORT = 3003
--- part9/first-steps-typescript/trainingCalculator.ts
+++ part9/first-steps-typescript/trainingCalculator.ts
@@ -8,22 +8,24 @@ interface Result {
   average: number
 }
 
-const calculateExercises = (target: number, dailyHours: number[]): Result => {
+export const calculateExercises = (target: number, dailyHours: number[]): Result => {
   const periodLength = dailyHours.length
   const trainingDays = dailyHours.filter((hour) => hour > 0).length
-  const average = dailyHours.reduce((a, b) => a + b) / periodLength
+  const average = dailyHours.reduce((a, b) => a + b, 0) / periodLength
   const success = average >= target
 
-  const getRating = (target: number, average: number): number => {
+  const getRating = (target: number, average: number): number  => {
     if (average < target * 0.8) return 1
     if (average < target) return 2
     if (average >= target) return 3
+    else return 0
   }
 
-  const getratingDescription = (rating: number) => {
-    if (rating === 1) return 'Not good. Do your best.'
+  const getratingDescription = (rating: number): string => {
+    if (rating === 1) return 'Not enough. Do your best.'
     if (rating === 2) return 'Not too bad but could be better'
     if (rating === 3) return 'Well done. Keep it up!'
+    else return 'An unexpected error occurred'
   }
 
   const rating = getRating(target, average)
@@ -40,6 +42,7 @@ const calculateExercises = (target: number, dailyHours: number[]): Result => {
   }
 }
 
+// eslint-disable-next-line @typescript-eslint/no-inferrable-types
 const daily: number = Number(process.argv[2])
 const dailyHours: number[] = process.argv.slice(3).map((hour) => Number(hour))
 
